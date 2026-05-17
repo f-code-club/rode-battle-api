ddc6d992aa47dcaeeb6c95a9052366edc7286e54
Huy Phong <touman876@gmail.com>
2026-05-17 16:52:15 +0700

    feat(api): add login request and response models

--- internal/http/auth.dto.go
+++ internal/http/auth.dto.go
@@ -16,7 +16,19 @@ type RegisterRequest struct {
 	PhoneNumber string `json:"phone_number" validate:"required,numeric,min=10,max=11"`
 }
 
+type LoginRequest struct {
+	Email    string `json:"email" validate:"required,email"`
+	Password string `json:"password" validate:"required"`
+}
+
+type LoginResponse struct {
+	AccessToken string           `json:"access_token"`
+	UserProfile UserBasicProfile `json:"user_basic_profile"`
+}
+
 var _ fuego.InTransformer = (*RegisterRequest)(nil)
+var _ fuego.InTransformer = (*LoginRequest)(nil)
+var _ fuego.OutTransformer = (*LoginResponse)(nil)
 
 func (r *RegisterRequest) InTransform(ctx context.Context) error {
 	r.Email = strings.TrimSpace(
@@ -27,5 +39,18 @@ func (r *RegisterRequest) InTransform(ctx context.Context) error {
 
 	r.School = strings.TrimSpace(r.School)
 
+	return nil
+}
+
+func (l *LoginRequest) InTransform(ctx context.Context) error {
+	l.Email = strings.TrimSpace(
+		strings.ToLower(l.Email),
+	)
+
+	return nil
+}
+
+func (l *LoginResponse) OutTransform(ctx context.Context) error {
+	l.AccessToken = strings.TrimSpace(l.AccessToken)
 	return nil
 }
---
+++ internal/http/user.dto.go
@@ -0,0 +1,6 @@
+package http
+
+type UserBasicProfile struct {
+	Email string `json:"email"`
+	Name  string `json:"name"`
+}

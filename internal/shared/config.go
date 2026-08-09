8560a278b8a7c3a0631b7842774b33fcd8b34085
akagiyuu <khanghuynh0245@gmail.com>
2026-08-09 17:07:27 +0700

    feat(config): add jwt

--- internal/shared/config.go
+++ internal/shared/config.go
@@ -1,7 +1,11 @@
 package shared
 
 type Config struct {
-	Port        int    `env:"PORT" envDefault:"3000"`
-	DatabaseURL string `env:"DATABASE_URL"`
-	CorsOrigin  string `env:"CORS_ORIGIN" envDefault:"*"`
+	Port                int    `env:"PORT" envDefault:"3000"`
+	DatabaseURL         string `env:"DATABASE_URL"`
+	CorsOrigin          string `env:"CORS_ORIGIN" envDefault:"*"`
+	JWTRefreshSecret    string `env:"JWT_REFRESH_SECRET" envDefault:"secret"`
+	JWTRefreshExpiredIn int    `env:"JWT_REFRESH_EXPIRED_IN" envDefault:"86400"`
+	JWTSecret           string `env:"JWT_SECRET" envDefault:"secret"`
+	JWTExpiredIn        int    `env:"JWT_EXPIRED_IN" envDefault:"600"`
 }

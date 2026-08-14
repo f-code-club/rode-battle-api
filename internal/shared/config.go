fb7c15a19bda3435c71a27cab42ab5d2649a7645
Huy Phong <touman876@gmail.com>
2026-08-14 22:38:06 +0700

    refactor: update email environment configuration

--- internal/shared/config.go
+++ internal/shared/config.go
@@ -8,8 +8,8 @@ type Config struct {
 	JWTRefreshExpiredIn int    `env:"JWT_REFRESH_EXPIRED_IN" envDefault:"86400"`
 	JWTAccessSecret     string `env:"JWT_ACCESS_SECRET" envDefault:"secret"`
 	JWTAccessExpiredIn  int    `env:"JWT_ACCESS_EXPIRED_IN" envDefault:"600"`
-	MailerUsername      string `env:"MAIL_USERNAME" envDefault:"admin@example.com"`
-	MailerPassword      string `env:"MAIL_PASSWORD" envDefault:"password"`
-	MailerHostName      string `env:"MAIL_HOST_NAME" envDefault:"smtp.gmail.com"`
-	MailerPortNumber    string `env:"MAIL_PORT_NUMBER" envDefault:"587"`
+	MailerUsername      string `env:"EMAIL_USERNAME"`
+	MailerPassword      string `env:"EMAIL_PASSWORD"`
+	MailerHostName      string `env:"EMAIL_HOST_NAME" envDefault:"smtp.gmail.com"`
+	MailerPortNumber    string `env:"EMAIL_PORT_NUMBER" envDefault:"587"`
 }

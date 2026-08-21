726f738c568078d5eb903d7d1f6dfdc5b0ba841f
patohru <patohru2006@gmail.com>
2026-08-21 14:52:12 +0700

    feat: add create submission to transport http

--- internal/problems/transport/http/server.go
+++ internal/problems/transport/http/server.go
@@ -1,11 +1,16 @@
 package http
 
 import (
+	"github.com/getkin/kin-openapi/openapi3"
 	"github.com/go-fuego/fuego"
+	"github.com/go-fuego/fuego/option"
 	"github.com/jackc/pgx/v5/pgxpool"
+
 	"github.com/f-code-club/rode-battle-api/internal/problems/service"
 	"github.com/f-code-club/rode-battle-api/internal/shared"
+	"github.com/f-code-club/rode-battle-api/internal/shared/middleware"
 )
+
 type Server struct {
 	service        service.Service
 	accessTokenSvc *shared.TokenService
@@ -22,8 +27,12 @@ func NewServer(
 }
 
 func (s *Server) RegisterRoutes(f *fuego.Server) {
+	m := middleware.ParseTokenMiddlewareBuilder{Service: s.accessTokenSvc}.Middleware
 
 	g := fuego.Group(f, "/problems")
 	fuego.Get(g, "/{id}", s.GetProblem)
-
+	fuego.Post(g, "/{id}/submit", s.CreateSubmission,
+		option.Middleware(m),
+		option.Security(openapi3.SecurityRequirement{"bearerAuth": []string{}}),
+	)
 }

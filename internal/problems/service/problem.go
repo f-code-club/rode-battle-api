abac7db67f747e0aa131e2a8ea2679555a69716f
patohru <patohru2006@gmail.com>
2026-08-21 14:52:46 +0700

    feat: add get submit history to service

--- internal/problems/service/problem.go
+++ internal/problems/service/problem.go
@@ -3,13 +3,17 @@ package service
 import (
 	"context"
 	"net/http"
+	"time"
 
 	"github.com/f-code-club/rode-battle-api/internal/problems/repository"
-	"github.com/f-code-club/rode-battle-api/internal/shared/errors"
+	"github.com/f-code-club/rode-battle-api/internal/shared"
+	apperr "github.com/f-code-club/rode-battle-api/internal/shared/errors"
 	"github.com/google/uuid"
 )
 
 type Language = repository.Language
+
+type Verdict = repository.Verdict
 
 type Problem struct {
 	Position    int32      `json:"position"`
@@ -20,17 +24,26 @@ type Problem struct {
 	Languages   []Language `json:"languages"`
 }
 
+type ProblemHistory struct {
+	ID        uuid.UUID `json:"id"`
+	Language  Language  `json:"language"`
+	Code      string    `json:"code"`
+	Verdict   Verdict   `json:"verdict"`
+	Score     float32   `json:"score"`
+	CreatedAt time.Time `json:"created_at"`
+}
+
 func (s *Service) GetProblem(ctx context.Context, id uuid.UUID) (*Problem, error) {
 	queries := repository.New(s.pool)
 
 	problem, err := queries.GetProblem(ctx, id)
 	if err != nil {
-		return nil, errors.Wrap(http.StatusBadRequest, "Failed to get problem", err)
+		return nil, apperr.Wrap(http.StatusBadRequest, "Failed to get problem", err)
 	}
 
 	languages, err := queries.GetProblemLanguages(ctx, id)
 	if err != nil {
-		return nil, errors.Wrap(http.StatusBadRequest, "Failed to get language", err)
+		return nil, apperr.Wrap(http.StatusBadRequest, "Failed to get language", err)
 	}
 
 	return &Problem{
@@ -41,4 +54,28 @@ func (s *Service) GetProblem(ctx context.Context, id uuid.UUID) (*Problem, error
 		MemoryLimit: *problem.MemoryLimit,
 		Languages:   languages,
 	}, nil
+}
+
+func (s *Service) GetSubmitHistory(ctx context.Context, problemID uuid.UUID) ([]ProblemHistory, error) {
+	queries := repository.New(s.pool)
+
+	rows, err := queries.GetSubmitHistory(ctx, problemID)
+	if err != nil {
+		return nil, apperr.Wrap(http.StatusBadRequest, "Problem not found", err)
+	}
+
+	history := make([]ProblemHistory, 0, len(rows))
+	println(len(history))
+	for _, row := range rows {
+		history = append(history, ProblemHistory{
+			ID:        row.ID,
+			Language:  row.Language,
+			Code:      row.Code,
+			Verdict:   shared.Deref(row.Verdict),
+			Score:     shared.Deref(row.Score),
+			CreatedAt: row.CreatedAt,
+		})
+	}
+
+	return history, nil
 }

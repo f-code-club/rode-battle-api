6ed24abb06b0f705eb24f21dc7f1858b48ad277e
Huy Phong <touman876@gmail.com>
2026-08-30 15:35:07 +0700

    refactor(auth): simplify logout to only clear refresh token cookie

--- internal/auth/transport/http/logout.go
+++ internal/auth/transport/http/logout.go
@@ -7,22 +7,6 @@ import (
 )
 
 func (s *Server) Logout(c fuego.ContextNoBody) (string, error) {
-	refreshToken, err := c.Cookie(refreshTokenCookie)
-	if err != nil {
-		return "", fuego.UnauthorizedError{
-			Err:    err,
-			Detail: "refresh token not found",
-		}
-	}
-
-	err = s.service.Logout(c.Context(), refreshToken.Value)
-	if err != nil {
-		return "", fuego.UnauthorizedError{
-			Err:    err,
-			Detail: "invalid refresh token",
-		}
-	}
-
 	c.SetCookie(http.Cookie{
 		Name:   refreshTokenCookie,
 		Value:  "",

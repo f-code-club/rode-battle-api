f90c77cae381f746d3734356b3c6c1d84fce74a3
Huy Phong <touman876@gmail.com>
2026-09-04 00:10:38 +0700

    feat(contest): add /contests handler

---
+++ internal/contests/transport/http/create.go
@@ -0,0 +1,24 @@
+package http
+
+import (
+	"github.com/f-code-club/rode-battle-api/internal/contests/service"
+	"github.com/go-fuego/fuego"
+	"github.com/google/uuid"
+)
+
+func (s *Server) CreateContest(c fuego.ContextWithBody[service.CreateContestRequest]) (uuid.UUID, error) {
+	req, err := c.Body()
+	if err != nil {
+		return uuid.Nil, fuego.BadRequestError{
+			Title: "Invalid request body",
+			Err:   err,
+		}
+	}
+
+	contestID, err := s.service.CreateContest(c.Context(), req)
+	if err != nil {
+		return uuid.Nil, err
+	}
+
+	return contestID, nil
+}

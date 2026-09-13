a593dd2e6bbbc64688e8eabb7f17744cf84067dd
patohru <patohru2006@gmail.com>
2026-09-13 13:55:00 +0700

    feat: add s3 to problem service/server

--- internal/problems/service/service.go
+++ internal/problems/service/service.go
@@ -1,13 +1,15 @@
 package service
 
 import (
+	"github.com/f-code-club/rode-battle-api/internal/shared"
 	"github.com/jackc/pgx/v5/pgxpool"
 )
 
 type Service struct {
 	pool *pgxpool.Pool
+	s3   *shared.S3Service
 }
 
-func New(pool *pgxpool.Pool) Service {
-	return Service{pool}
+func New(pool *pgxpool.Pool, s3 *shared.S3Service) Service {
+	return Service{pool, s3}
 }

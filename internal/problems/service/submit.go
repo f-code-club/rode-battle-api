7dc3bc84f92e1a036f7aef8da77f36be5a5c8ca0
patohru <patohru2006@gmail.com>
2026-09-14 22:24:31 +0700

    feat: send submission id to rabbit queue for judge

--- internal/problems/service/submit.go
+++ internal/problems/service/submit.go
@@ -4,16 +4,21 @@ import (
 	"context"
 	"errors"
 	"net/http"
+	"time"
 
+	"github.com/caarlos0/env/v11"
 	"github.com/f-code-club/rode-battle-api/internal/problems/repository"
+	"github.com/f-code-club/rode-battle-api/internal/shared"
 	apperr "github.com/f-code-club/rode-battle-api/internal/shared/errors"
 	"github.com/google/uuid"
 	"github.com/jackc/pgx/v5/pgconn"
+	amqp "github.com/rabbitmq/amqp091-go"
 )
 
 type CreateSubmission = repository.CreateSubmissionParams
 
 func (s *Service) CreateSubmission(ctx context.Context, problemID uuid.UUID, accountID uuid.UUID, language Language, code string) (uuid.UUID, error) {
+	cfg, err := env.ParseAs[shared.Config]()
 	var pgErr *pgconn.PgError
 	queries := repository.New(s.pool)
 
@@ -29,7 +34,27 @@ func (s *Service) CreateSubmission(ctx context.Context, problemID uuid.UUID, acc
 	if err != nil {
 		return uuid.Nil, apperr.Wrap(http.StatusBadRequest, "Failed to create submission", err)
 	}
-	// Add queue later
+	q, err := s.channel.QueueDeclare(
+		cfg.TaskQueue,
+		true,
+		false,
+		false,
+		false,
+		amqp.Table{
+			amqp.QueueTypeArg: amqp.QueueTypeQuorum,
+		},
+	)
+	if err != nil {
+		return uuid.Nil, apperr.Wrap(http.StatusInternalServerError, "Failed to declare a queue", err)
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	err = s.channel.PublishWithContext(ctx, "", q.Name, false, false, amqp.Publishing{
+		ContentType: "text/plain",
+		Body:        []byte(submissionID[:]),
+	})
 
 	return submissionID, nil
 }

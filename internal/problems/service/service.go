5552de38782286d05666c8c3d3f4200317663e29
patohru <patohru2006@gmail.com>
2026-09-15 22:30:45 +0700

    feat: add amqp service

--- internal/problems/service/service.go
+++ internal/problems/service/service.go
@@ -3,15 +3,14 @@ package service
 import (
 	"github.com/f-code-club/rode-battle-api/internal/shared"
 	"github.com/jackc/pgx/v5/pgxpool"
-	amqp "github.com/rabbitmq/amqp091-go"
 )
 
 type Service struct {
-	pool    *pgxpool.Pool
-	s3      *shared.S3Service
-	channel *amqp.Channel
+	pool *pgxpool.Pool
+	s3   *shared.S3Service
+	amqp *shared.AmqpService
 }
 
-func New(pool *pgxpool.Pool, s3 *shared.S3Service, channel *amqp.Channel) Service {
+func New(pool *pgxpool.Pool, s3 *shared.S3Service, channel *shared.AmqpService) Service {
 	return Service{pool, s3, channel}
 }

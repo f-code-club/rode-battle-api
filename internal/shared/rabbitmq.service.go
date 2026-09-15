c650735d680de022696a33f8219f3a6b7c3bfbbc
patohru <patohru2006@gmail.com>
2026-09-15 22:29:58 +0700

    feat: create amqp service with channel and queue

--- internal/shared/rabbitmq.service.go
+++ internal/shared/rabbitmq.service.go
@@ -4,7 +4,12 @@ import (
 	amqp "github.com/rabbitmq/amqp091-go"
 )
 
-func NewRabbitChannel(url string) (*amqp.Channel, error) {
+type AmqpService struct {
+	Channel *amqp.Channel
+	Queue   amqp.Queue
+}
+
+func NewRabbitChannel(url string, taskQueue string) (*AmqpService, error) {
 	conn, err := amqp.Dial(url)
 	if err != nil {
 		return nil, err
@@ -15,5 +20,22 @@ func NewRabbitChannel(url string) (*amqp.Channel, error) {
 		return nil, err
 	}
 
-	return ch, nil
+	q, err := ch.QueueDeclare(
+		taskQueue,
+		true,
+		false,
+		false,
+		false,
+		amqp.Table{
+			amqp.QueueTypeArg: amqp.QueueTypeQuorum,
+		},
+	)
+	if err != nil {
+		return nil, err
+	}
+
+	return &AmqpService{
+		Channel: ch,
+		Queue:   q,
+	}, nil
 }
